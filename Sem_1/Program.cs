54a100a080a8b315fcf9f8b197b3a93255a24249
Alexey-Ponamarev <146445792+Alexey-Ponamarev@users.noreply.github.com>
2023-10-13 19:34:22 +0300

    Возвести число в квадрат_Дополненный вариант

--- Sem_1/Program.cs
+++ Sem_1/Program.cs
@@ -1,5 +1,8 @@
-﻿Console.Clear();
-int number =-8;
+﻿using System.Diagnostics;
+
+Console.Clear();
+Console.Write("Введите число:  ");
+int number =Convert.ToInt32(Console.ReadLine());
 
 int Results=number*number;
 Console.Write(Results);

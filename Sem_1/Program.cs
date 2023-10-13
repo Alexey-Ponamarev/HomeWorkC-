e375ec202eb5368ba0d2909229c9af7d95bd7063
Alexey-Ponamarev <146445792+Alexey-Ponamarev@users.noreply.github.com>
2023-10-13 21:50:48 +0300

    Вывод всех отрицательных и положительных занчений между N и -N

--- Sem_1/Program.cs
+++ Sem_1/Program.cs
@@ -1,40 +1,20 @@
-﻿// 3. Напишите программу, которая
-// будет выдавать название дня недели
-// по заданному номеру.
-// 3 -> Среда
-// 5 -> Пятница
-Console.Clear();
-Console.WriteLine("Введите день недели ");
-int NumberOfDay = Convert.ToInt32(Console.ReadLine());
-if (NumberOfDay == 1)
+﻿// 5. Напишите программу, которая на вход принимает
+// одно число (N), а на выходе показывает
+// все целые числа в промежутке от -N до N.
+// 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
+// 2 -> " -2, -1, 0, 1, 2"
+
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+if (N < 0)
 {
-    Console.WriteLine("Понедельник");
+N *= -1; // N = N * -1
+Console.WriteLine("Вы ввели отрц. число N, я умножил его на -1 :)");
 }
-else if (NumberOfDay == 2)
+int negativeN = N * -1; // -N
+
+while (negativeN <= N)
 {
-    Console.WriteLine("Вторник");
-}
-else if (NumberOfDay == 3)
-{
-    Console.WriteLine("Среда");
-}
-else if (NumberOfDay == 4)
-{
-    Console.WriteLine("Четверг");
-}
-else if (NumberOfDay == 5)
-{
-    Console.WriteLine("Пятица");
-}
-else if (NumberOfDay == 6)
-{
-    Console.WriteLine("Суббота");
-}
-else if (NumberOfDay == 7)
-{
-    Console.WriteLine("Воскресенье");
-}
-else //Все остальные случаи
-{
-    Console.WriteLine("Нет такого дня недели");
+Console.Write(negativeN + " ");
+negativeN += 1; // negativeN = negativeN + 1
 }

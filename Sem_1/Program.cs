bf867e31c7fba9ac0c0c41e36fe817e422fa9da1
Alexey-Ponamarev <146445792+Alexey-Ponamarev@users.noreply.github.com>
2023-10-13 20:29:11 +0300

    Вывод названия дня недели

--- Sem_1/Program.cs
+++ Sem_1/Program.cs
@@ -1,8 +1,40 @@
-﻿using System.Diagnostics;
-
+﻿// 3. Напишите программу, которая
+// будет выдавать название дня недели
+// по заданному номеру.
+// 3 -> Среда
+// 5 -> Пятница
 Console.Clear();
-Console.Write("Введите число:  ");
-int number =Convert.ToInt32(Console.ReadLine());
-
-int Results=number*number;
-Console.Write("Квадрат от числа " + number +" = "+Results);
+Console.WriteLine("Введите день недели ");
+int NumberOfDay = Convert.ToInt32(Console.ReadLine());
+if (NumberOfDay == 1)
+{
+    Console.WriteLine("Понедельник");
+}
+else if (NumberOfDay == 2)
+{
+    Console.WriteLine("Вторник");
+}
+else if (NumberOfDay == 3)
+{
+    Console.WriteLine("Среда");
+}
+else if (NumberOfDay == 4)
+{
+    Console.WriteLine("Четверг");
+}
+else if (NumberOfDay == 5)
+{
+    Console.WriteLine("Пятица");
+}
+else if (NumberOfDay == 6)
+{
+    Console.WriteLine("Суббота");
+}
+else if (NumberOfDay == 7)
+{
+    Console.WriteLine("Воскресенье");
+}
+else //Все остальные случаи
+{
+    Console.WriteLine("Нет такого дня недели");
+}

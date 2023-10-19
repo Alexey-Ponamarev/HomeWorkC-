0ee1bbd6009614efc7c4e4b7d9cf864b8a338f3a
Alexey-Ponamarev <146445792+Alexey-Ponamarev@users.noreply.github.com>
2023-10-19 17:56:30 +0300

    Задача 15. Определение выходных и рабочих дней по номеру дня

---
+++ Homework_2/Задача 15/Program.cs
@@ -0,0 +1,28 @@
+﻿
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Clear(); // Очищаем консоль от предыдущих команд
+        Console.WriteLine("Введите номер дня недели: "); // Вводим номер дня недели
+
+        // Проверяем что введена цифра
+        string StringNumber = Console.ReadLine();
+        int NumberDay; // Объевляем переменную чтобы потом в нее положить значение
+        bool isNumber = int.TryParse(StringNumber, out NumberDay);
+
+        if (isNumber && StringNumber.Length ==1 && NumberDay>5 && NumberDay<8 && NumberDay>0) //Если условие истина
+        {
+            Console.WriteLine($"День недели {StringNumber} - выходной день"); // Вывод результата
+        }
+        else if(isNumber && StringNumber.Length ==1 && NumberDay<=5 && NumberDay<8 && NumberDay>0)
+        {
+          Console.WriteLine($"День недели {StringNumber} - рабочий день"); // Вывод результата  
+        }
+        else
+        {
+            Console.WriteLine("Данные введены не корректно"); // Вывод результата
+        }
+    }
+
+}

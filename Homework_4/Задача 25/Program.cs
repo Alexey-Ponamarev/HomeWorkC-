ba5594c031b50d8cc6a769aaefaba4afbb169f47
Alexey-Ponamarev <Alexey.Ponamarev@mail.ru>
2023-10-25 18:40:48 +0300

    ДЗ №4.Задача №27, сложить все цифры в числе

--- Homework_4/Задача 25/Program.cs
+++ Homework_4/Задача 25/Program.cs
@@ -3,8 +3,12 @@ Console.WriteLine("Введите значение для А: ");
 int numberA=Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите значение для B: ");
 int numberB=Convert.ToInt32(Console.ReadLine());
-int result=0;
+
+Console.Write($"Число {numberA} в степени {numberB} = {NumbPow(numberA, numberB)}");
+int NumbPow(int A, int B)
+{int result=0;
 for (int i=1; i<=numberB; i++) {
 result = result+numberA;
 }
-Console.WriteLine($"Число {numberA} в степени {numberB} = {result}");
+return result;
+}
---
+++ Homework_4/Задача 27/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"Сумма всех цифр числа {number} равна {GetSumm(number)}");
+
+int GetSumm(int A)
+{
+    int NumbInteger;
+    int NumbRemaind;
+    int summ = 0;
+    do
+    {
+        NumbInteger = A / 10;
+        NumbRemaind = A % 10;
+        summ = summ + NumbRemaind;
+        A = NumbInteger;
+    } while (NumbInteger > 0);
+    return summ;
+}

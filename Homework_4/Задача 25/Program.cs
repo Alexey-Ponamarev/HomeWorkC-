65c4847a755e5ba9f8f91c9bd263edf19c3838ad
Alexey-Ponamarev <146445792+Alexey-Ponamarev@users.noreply.github.com>
2023-10-25 17:38:34 +0300

    ДЗ 4. Задача №25. Возвести число в степень по средством цикла

---
+++ Homework_4/Задача 25/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.WriteLine("Введите значение для А: ");
+int numberA=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение для B: ");
+int numberB=Convert.ToInt32(Console.ReadLine());
+int result=0;
+for (int i=1; i<=numberB; i++) {
+result = result+numberA;
+}
+Console.WriteLine($"Число {numberA} в степени {numberB} = {result}");
